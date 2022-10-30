6787f91ff906ad015465a98caad3baf9ff07b2b9
bioodev <bioodev@protonmail.com>
2022-10-30 02:20:40 -0300

    .

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import './App.css';
 import ReactPlayer from 'react-player/lazy';
 import FancyText from '@carefully-coded/react-text-gradient';
 
-
-
 function App() {
   return (
     <div className='container-lg flex justify-center items-center h-screen w-screen select-none cursor-crosshair'>
-      <div className="App">
-        <div className='rounded-2xl'>
+      <div className="App shadow-2xl">
+        <div className='rounded-xl overflow-hidden '>
           <div className='header'>
-            <div className='fancytext font-extrabold font-mono text-3xl bg-black flex justify-center items-center p-5 pb-0 rounded-t-xl'>
+            <div className='fancytext -space-x-0.5 font-extrabold font-mono text-3xl bg-black flex justify-center items-center p-5 pb-0'>
               <FancyText
                 gradient={{ from: '#F858E0', to: '#77156C', type: 'linear' }}
                 animateTo={{ from: '#6DEDD0', to: '#7AE23A' }}
@@ -20,7 +18,7 @@ function App() {
               </FancyText>
             </div>
           </div>
-          <div className='player flex rounded-b-xl overflow-hidden justify-center items-center'>
+          <div className='player flex overflow-hidden justify-center items-center'>
             <ReactPlayer controls="true" width="400px" url='https://www.youtube.com/playlist?list=PLfR3SclqQjGK6Qwk6VIqmrLrJsHjv3OES' />
           </div>
 
